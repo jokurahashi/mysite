184e035c196279339d28b6e0989a39a099923b41
j.kurahashi <j.kurahashi@zooops-japan.co.jp>
2025-10-03 14:32:56 +0900

    ユーザー一覧・詳細に作成日時と更新日時の作成

--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.shortcuts import render, redirect
-from django.contrib.auth import login
 from .forms import CustomUserCreationForm
 from .forms import CustomUserEditForm
 from .models import CustomUser
 from django.contrib import messages
+
 
 # ユーザー登録ビュー
 def signup(request):
@@ -29,19 +29,30 @@ def signup(request):
 # ユーザー一覧ビュー
 def user_list(request):
     users = CustomUser.objects.all()
+    users = CustomUser.objects.order_by("-created_at")  # ← 作成日時の降順
+    users = CustomUser.objects.all().order_by("-updated_at")  # 更新日時の降順
     return render(request, "accounts/user_list.html", {"users": users})
 
+
 from .forms import CustomUserUpdateForm
+
 
 # ユーザー詳細
 def user_detail(request, pk):
     user = get_object_or_404(CustomUser, pk=pk)
-    return render(request, "accounts/user_detail.html", {"user": user, })
+    return render(
+        request,
+        "accounts/user_detail.html",
+        {
+            "user": user,
+        },
+    )
+
 
 # ユーザー編集
 def user_edit(request, pk):
     user = get_object_or_404(CustomUser, pk=pk)
-    if request.method == "POST":
+    if request.method == "POST":  # フォームが送信された場合
         form = CustomUserEditForm(request.POST, instance=user)
         if form.is_valid():
             form.save()
@@ -50,6 +61,7 @@ def user_edit(request, pk):
     else:
         form = CustomUserEditForm(instance=user)
     return render(request, "accounts/user_edit.html", {"form": form, "user": user})
+
 
 # ユーザー削除(論理削除)
 def user_delete(request, pk):
